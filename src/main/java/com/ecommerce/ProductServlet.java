7e2e45e536eace382f6a3003f6941baf92a2d52c
parimal <parimal@gmail.com>
2026-07-30 19:58:40 +0000

    pp

--- src/main/java/com/ecommerce/ProductServlet.java
+++ src/main/java/com/ecommerce/ProductServlet.java
@@ -8,7 +8,7 @@ public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        res.setContentType("text/html");
+        res.setContentType("text/html; charset=UTF-8");
         PrintWriter out = res.getWriter();
 
         out.println("<html>");
