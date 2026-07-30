d20f69e3379842d4b9cb26cca90ff91b13be3294
parimal <parimal@gmail.com>
2026-07-30 19:19:48 +0000

    third commit

--- src/main/java/com/ecommerce/ProductServlet.java
+++ src/main/java/com/ecommerce/ProductServlet.java
@@ -1,19 +1,20 @@
 package com.ecommerce;
 
 import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
 
         out.println("<h1>Product List</h1>");
         out.println("<ul>");
-        out.println("<li>iPhone - ₹80,000</li>");
-        out.println("<li>Laptop - ₹60,000</li>");
+        out.println("<li>iPhone</li>");
+        out.println("<li>Laptop</li>");
         out.println("</ul>");
     }
 }
