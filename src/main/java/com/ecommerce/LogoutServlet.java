dfd6411f8b032c2f9e81f553d42e4b5973a9bc57
parimallpradhan <153976296+parimallpradhan@users.noreply.github.com>
2026-08-09 02:40:54 +0530

    Create LogoutServlet.java

---
+++ src/main/java/com/ecommerce/LogoutServlet.java
@@ -0,0 +1,22 @@
+package com.ecommerce;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate(); // destroy session
+        }
+
+        response.sendRedirect("login.jsp");
+    }
+}
